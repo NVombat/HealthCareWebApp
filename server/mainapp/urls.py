11ce2140c5d3bae0b450edfe0c24003ca4ef4be2
NVombat <nvombatkere@gmail.com>
2021-10-07 12:06:12 +0530

    :construction: fix paths

--- server/mainapp/urls.py
+++ server/mainapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home, name='Home'),
+    path('login/', views.login, name='Login'),
+    path('signup/', views.signup, name='Signup'),
 ]
--- server/mainapp/views.py
+++ server/mainapp/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "index.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def signup(request):
+    return render(request, "signup.html")
